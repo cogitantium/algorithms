060cf2c3ed3a4d01eff6227107b64da18d221bc4
cogitantium <falke.cs@gmail.com>
2019-05-10 21:52:46 +0200

    Add docstring to insertion-sort

--- algorithms/insertionsort.py
+++ algorithms/insertionsort.py
@@ -1,4 +1,12 @@
 def insertion_sort(input_list):
+    """
+    Insertion-sorts input by iterating over index 1 to input-length and for each setting a key which is to be
+    inserted, this key is then inserted towards the beginning of the list, if the list is not yet exhausted and the
+    next item is greater than the key.
+
+    :param input_list: the input to sort
+    :return: the input sorted in-place
+    """
     for j in range(1, len(input_list)):
         key = input_list[j]
         i = j - 1
