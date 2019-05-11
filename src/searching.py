b5c75ec88aefd73fdadf678649137380e7a80583
cogitantium <falke.cs@gmail.com>
2019-05-11 14:34:04 +0200

    Generalise searching runner

--- src/searching.py
+++ src/searching.py
@@ -3,6 +3,10 @@ import random
 from src.algorithms.searching.binarysearch import binary_search
 
 
+def setup_binary_search(array, key):
+    return [array, 0, len(array) + 1, key]
+
+
 def run_algorithm(algorithm):
     """
     Runs algorithm and prints debug information on success and input and output
@@ -19,17 +23,22 @@ def run_algorithm(algorithm):
     for i in range(20):
         array.append(random.randint(0, 99))
 
-    # Create set from list
-    array_set = set(array)
-
-    # Recreate list from set and sort it
-    array = sorted(list(array_set))
+    # Create set to remove duplicates and recreate list from set and sort it
+    array = sorted(list(set(array)))
 
     # Set value to search for
     key = array[12]
 
-    # Call function
-    return_val = algorithm(array, 0, len(array) + 1, key)
+    print("Searching for value", key, "at index", 12, "in list", array)
+
+    # Setup algorithm
+    if algorithm is binary_search:
+        args = setup_binary_search(array, key)
+    else:
+        raise ValueError("Could not match algorithm to any function arguments")
+
+    # Call function and unwrap list of args
+    return_val = algorithm(*args)
 
     # Print results and linebreak
     if key == array[return_val]:
